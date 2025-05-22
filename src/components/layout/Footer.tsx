56d9ab12bbcb7588ff5c93e3fe4587e517c5d717
Akshath Pandey <21951a0504@iare.ac.in>
2025-05-22 22:30:59 +0530

    khatam tata tata goodbye

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -12,22 +12,35 @@ const Footer = () => {
             <p className="text-gray-300 mb-4">
               Your personal finance tracker. Take control of your money and achieve your financial goals.
             </p>
-            <div className="flex space-x-4">
-              <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="text-white hover:text-yellow transition-colors">
-                <div className="w-8 h-8 flex items-center justify-center rounded-full border border-white">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                    <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
-                  </svg>
-                </div>
-              </a>
-              <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="text-white hover:text-yellow transition-colors">
-                <div className="w-8 h-8 flex items-center justify-center rounded-full border border-white">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
-                  </svg>
-                </div>
-              </a>
-            </div>
+            <div className="flex gap-2">
+  {/* Instagram */}
+  <a href="https://www.instagram.com/akshat___pandey07/" target="_blank" rel="noopener noreferrer">
+    <div className="w-8 h-8 flex items-center justify-center rounded-full border border-white text-white hover:bg-black hover:text-yellow transition-colors duration-300">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="16" height="16" viewBox="0 0 24 24">
+        <path d="M12 2.2c3.2 0 3.584.012 4.85.07 1.17.055 1.968.24 2.43.4a4.9 4.9 0 0 1 1.78 1.15 4.9 4.9 0 0 1 1.15 1.78c.16.462.345 1.26.4 2.43.058 1.266.07 1.65.07 4.85s-.012 3.584-.07 4.85c-.055 1.17-.24 1.968-.4 2.43a4.9 4.9 0 0 1-1.15 1.78 4.9 4.9 0 0 1-1.78 1.15c-.462.16-1.26.345-2.43.4-1.266.058-1.65.07-4.85.07s-3.584-.012-4.85-.07c-1.17-.055-1.968-.24-2.43-.4a4.9 4.9 0 0 1-1.78-1.15 4.9 4.9 0 0 1-1.15-1.78c-.16-.462-.345-1.26-.4-2.43C2.212 15.784 2.2 15.4 2.2 12s.012-3.584.07-4.85c.055-1.17.24-1.968.4-2.43a4.9 4.9 0 0 1 1.15-1.78 4.9 4.9 0 0 1 1.78-1.15c.462-.16 1.26-.345 2.43-.4C8.416 2.212 8.8 2.2 12 2.2zm0-2.2C8.735 0 8.332.012 7.052.07 5.748.128 4.757.318 3.94.603 3.102.894 2.388 1.304 1.706 1.986.938 2.755.528 3.47.237 4.308.05 4.996-.128 5.98-.186 7.288-.244 8.568-.256 8.97-.256 12c0 3.03.012 3.432.07 4.712.058 1.308.237 2.292.424 2.98.29.838.7 1.553 1.382 2.235.77.77 1.485 1.182 2.323 1.473.688.186 1.672.366 2.98.424C8.568 23.988 8.97 24 12 24s3.432-.012 4.712-.07c1.308-.058 2.292-.237 2.98-.424.838-.29 1.553-.7 2.235-1.382.77-.77 1.182-1.485 1.473-2.323.186-.688.366-1.672.424-2.98.058-1.28.07-1.682.07-4.712s-.012-3.432-.07-4.712c-.058-1.308-.237-2.292-.424-2.98a5.99 5.99 0 0 0-1.473-2.323A5.99 5.99 0 0 0 19.692.603C18.854.318 17.87.128 16.562.07 15.282.012 14.88 0 12 0zm0 5.838a6.162 6.162 0 1 0 0 12.324 6.162 6.162 0 0 0 0-12.324zm0 10.162a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm6.406-11.775a1.44 1.44 0 1 0 0 2.88 1.44 1.44 0 0 0 0-2.88z"/>
+      </svg>
+    </div>
+  </a>
+
+  {/* LinkedIn */}
+  <a href="https://www.linkedin.com/in/akshatpandey07/" target="_blank" rel="noopener noreferrer">
+    <div className="w-8 h-8 flex items-center justify-center rounded-full border border-white text-white hover:bg-black hover:text-yellow transition-colors duration-300">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="16" height="16" viewBox="0 0 24 24">
+        <path d="M4.983 3.5C3.879 3.5 3 4.38 3 5.483s.879 1.983 1.983 1.983h.02A1.986 1.986 0 0 0 7 5.484C7 4.38 6.12 3.5 5.017 3.5h-.034zm.017 4.749H3V21h2V8.25zM8.25 8.25H10.3v1.743h.028c.286-.54.986-1.107 2.03-1.107 2.17 0 2.57 1.43 2.57 3.288V21h-2V12.75c0-1.15-.02-2.628-1.6-2.628-1.6 0-1.844 1.25-1.844 2.543V21h-2V8.25z"/>
+      </svg>
+    </div>
+  </a>
+
+  {/* X / Twitter */}
+  <a href="https://x.com/akshath_pandey" target="_blank" rel="noopener noreferrer">
+    <div className="w-8 h-8 flex items-center justify-center rounded-full border border-white text-white hover:bg-black hover:text-yellow transition-colors duration-300">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="16" height="16" viewBox="0 0 24 24">
+        <path d="M13.14 10.728 22.225 0h-2.036l-7.8 9.57L5.96 0H0l9.41 13.527L0 24h2.036l8.186-10.045L18.273 24H24L13.14 10.728z"/>
+      </svg>
+    </div>
+  </a>
+</div>
+
           </div>
           
           <div>
@@ -52,7 +65,7 @@ const Footer = () => {
           <div>
             <h3 className="text-lg font-semibold mb-4">Contact</h3>
             <p className="text-gray-300 mb-2">appquery.team@gmail.com</p>
-            <p className="text-gray-300">123 Finance Street, Money City</p>
+            <p className="text-gray-300">Gurugram, Haryana</p>
           </div>
         </div>
         
